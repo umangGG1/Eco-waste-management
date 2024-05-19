4bb42c80386b654b4f96d5a4110004e4fa5e4e36
UmangGG13 <umang@plena.finance>
2024-05-19 13:23:35 +0530

    All Bhawans

--- eco-group/src/App.js
+++ eco-group/src/App.js
@@ -9,6 +9,17 @@ import Rajeev from './components/Bhawan Pages/rajeevBhawan';
 import ProtectedRoute from './components/protectedRoute';
 import FirstPage from './components/admin side/firstPage';
 import AdminPage from './components/admin side/adminPage';
+import Azad from './components/Bhawan Pages/azadBhawan';
+import Cautley from './components/Bhawan Pages/cautleyBhawan';
+import Ganga from './components/Bhawan Pages/gangaBhawan';
+import Govind from './components/Bhawan Pages/govindBhawan';
+import Jawahar from './components/Bhawan Pages/jawaharBhawan';
+import Kasturba from './components/Bhawan Pages/kasturbaBhawan';
+import Radhakrishnan from './components/Bhawan Pages/radhakrishnanBhawan';
+import Rajendra from './components/Bhawan Pages/rajendraBhawan';
+import Ravindra from './components/Bhawan Pages/ravindraBhawan';
+import Sarojini from './components/Bhawan Pages/sarojiniBhawan';
+import Vigyan from './components/Bhawan Pages/vigyanBhawan';
 
 const App = () => {
   const user = localStorage.getItem("token")
@@ -29,6 +40,52 @@ const App = () => {
           path="/scanner/rajeev"
           element={<Rajeev/>}
         />
+     <Route
+          path="/scanner/azad"
+          element={<Azad/>}
+        />
+      <Route
+          path="/scanner/cautley"
+          element={<Cautley/>}
+        />
+      <Route
+          path="/scanner/ganga"
+          element={<Ganga/>}
+        />
+      <Route
+          path="/scanner/govind"
+          element={<Govind/>}
+      />
+      <Route
+          path="/scanner/jawahar"
+          element={<Jawahar/>}
+      />
+      <Route
+          path="/scanner/kasturba"
+          element={<Kasturba/>}
+      />
+      <Route
+          path="/scanner/rkb"
+          element={<Radhakrishnan/>}
+      />
+      <Route
+          path="/scanner/rajendra"
+          element={<Rajendra/>}
+      />
+      <Route
+          path="/scanner/ravindra"
+          element={<Ravindra/>}
+      />
+      <Route
+          path="/scanner/sarojini"
+          element={<Sarojini/>}
+      />
+      <Route
+          path="/scanner/vigyan"
+          element={<Vigyan/>}
+      />
+
+             
     </Routes>
     
   );
---
+++ eco-group/src/components/Bhawan Pages/sarojiniBhawan.js
@@ -0,0 +1,81 @@
+import React from "react";
+import "./rajeevBhawan.css";
+import photo from "./sarojini.png";
+import { useNavigate } from 'react-router-dom';
+import { baseUrl } from "../../url";
+const Sarojini = () => {
+  const bhawanName = "Sarojini Bhawan"; // Define the bhawan name
+  const navigate= useNavigate();
+
+  const handleButtonClick = (status) => {
+    const data = {
+      bhawanName: bhawanName,
+      status: status,
+    };
+
+    fetch(`${baseUrl}/api/bhawan/store`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log('Success:', data);
+        navigate('/success');
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+        // Optionally, add logic to show error feedback to the user
+      });
+  };
+
+  return (
+    <div className="bhawanPage">
+      <div className="bhawanPhotoName">
+        <img src={photo} alt="Bhawan" />
+      </div>
+      <div className="buttons">
+        <button
+          type="button"
+          onClick={() => handleButtonClick('segregated')}
+          style={{
+            backgroundColor: "white",
+            color: "black",
+            height: "40px",
+            width: "222px",
+            margin: '10px',
+            borderRadius: '4px',
+            fontFamily: "Maven Pro",
+            fontWeight: '600',
+            border: "0px",
+            boxShadow: '0 3px 5px rgba(0, 0, 0, 0.4)',
+          }}
+        >
+          Segregated
+        </button>
+        <button
+          type="button"
+          onClick={() => handleButtonClick('unsegregated')}
+          style={{
+            backgroundColor: "black",
+            color: "white",
+            height: "40px",
+            width: "222px",
+            margin: '10px',
+            borderRadius: '4px',
+            fontFamily: "Maven Pro",
+            fontWeight: '500',
+            border: "0px",
+            boxShadow: '0 3px 5px rgba(0, 0, 0, 0.4)',
+          }}
+        >
+          Unsegregated
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sarojini;
